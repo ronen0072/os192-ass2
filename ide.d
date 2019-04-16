0218440e0b0b097c182a23001458e30c0c540af8
dalitroz <dalitrozin@gmail.com>
2019-04-16 23:35:12 +0300

    deleted all executables

--- ide.d
+++
@@ -1,3 +0,0 @@
-ide.o: ide.c /usr/include/stdc-predef.h types.h defs.h param.h \
- memlayout.h mmu.h kthread.h spinlock.h proc.h x86.h traps.h sleeplock.h \
- fs.h buf.h
--- pipe.d
+++
@@ -1,2 +0,0 @@
-pipe.o: pipe.c /usr/include/stdc-predef.h types.h defs.h param.h mmu.h \
- kthread.h spinlock.h proc.h fs.h sleeplock.h file.h
